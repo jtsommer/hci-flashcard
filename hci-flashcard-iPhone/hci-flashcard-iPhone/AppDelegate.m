a41d3c01d975c8b4b5fb886d6ffaf3aeeb49542a
Jordan Sommers <jtsommer@ucsc.edu>
2014-03-06 13:54:29 -0800

    Fixed MagicalRecord setup

--- hci-flashcard-iPhone/hci-flashcard-iPhone/AppDelegate.m
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/AppDelegate.m
@@ -13,6 +13,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // Setup Core Data with Magical Record
+    [MagicalRecord setupCoreDataStackWithStoreNamed:@"FlashcardModel"];
     return YES;
 }
 							
