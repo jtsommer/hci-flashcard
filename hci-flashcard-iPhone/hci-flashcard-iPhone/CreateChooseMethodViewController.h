fef5f61717e731c4dbd9ba5be0b8e999a13c18a7
Jordan Sommers <jtsommer@ucsc.edu>
2014-03-09 20:31:43 -0700

    Layout and added class for view controller

---
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/CreateChooseMethodViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateChooseMethodViewController.h
+//  hci-flashcard-iPhone
+//
+//  Created by Jordan Sommers on 3/9/14.
+//  Copyright (c) 2014 CMPE131. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateChooseMethodViewController : UIViewController
+
+@end
---
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/CreateChooseMethodViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreateChooseMethodViewController.m
+//  hci-flashcard-iPhone
+//
+//  Created by Jordan Sommers on 3/9/14.
+//  Copyright (c) 2014 CMPE131. All rights reserved.
+//
+
+#import "CreateChooseMethodViewController.h"
+
+@interface CreateChooseMethodViewController ()
+
+@end
+
+@implementation CreateChooseMethodViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
