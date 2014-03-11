6593d737edf3ee9084509c39d47ef5e66d68078a
Jordan Sommers <jtsommer@ucsc.edu>
2014-03-11 00:01:36 -0700

    Added StudyChooseMethod controller and reorganized the project group structure

---
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/StudyChooseMethodViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudyChooseMethodViewController.h
+//  hci-flashcard-iPhone
+//
+//  Created by Jordan Sommers on 3/10/14.
+//  Copyright (c) 2014 CMPE131. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudyChooseMethodViewController : UIViewController
+
+@end
---
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/StudyChooseMethodViewController.m
@@ -0,0 +1,38 @@
+//
+//  StudyChooseMethodViewController.m
+//  hci-flashcard-iPhone
+//
+//  Created by Jordan Sommers on 3/10/14.
+//  Copyright (c) 2014 CMPE131. All rights reserved.
+//
+
+#import "StudyChooseMethodViewController.h"
+
+@interface StudyChooseMethodViewController ()
+
+@end
+
+@implementation StudyChooseMethodViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
