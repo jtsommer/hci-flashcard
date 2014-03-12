bb6a51f757a21b428e01bfa48b8e573be3dc776a
Jordan Sommers <jtsommer@ucsc.edu>
2014-03-12 15:56:13 -0700

    Some flashcard view logic

--- hci-flashcard-iPhone/hci-flashcard-iPhone/FlashcardViewController.m
+++ hci-flashcard-iPhone/hci-flashcard-iPhone/FlashcardViewController.m
@@ -9,8 +9,12 @@
 #import "FlashcardViewController.h"
 
 @interface FlashcardViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
 
 @end
+
+const int FLASHCARD_STATE_FRONT = 1;
+const int FLASHCARD_STATE_BACK = 2;
 
 @implementation FlashcardViewController
 
@@ -27,6 +31,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+}
+
+#pragma mark Input Actions
+
+- (IBAction)donePressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)handleTap:(UITapGestureRecognizer *)sender {
+    if (sender.numberOfTapsRequired == 1) {
+        [self flipToFlashcardBack];
+    }
+}
+
+#pragma mark Flashcard Methods
+
+- (void)flipViewToSelf {
+    [UIView transitionWithView:self.view duration:1 options:UIViewAnimationOptionTransitionFlipFromLeft animations: nil completion:nil];
+}
+
+- (void)flipToFlashcardBack {
+    [self flipViewToSelf];
+    self.textLabel.text = @"changed";
+}
+
+- (void)flipToFlascardFront {
+    [self flipViewToSelf];
+    self.textLabel.text = @"Study";
 }
 
 - (void)didReceiveMemoryWarning
